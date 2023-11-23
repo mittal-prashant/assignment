a074c7fa3392f2e35b17e6ef7786c07561c4db37
Prashant Mittal <75663788+mittal-prashant@users.noreply.github.com>
2023-11-23 22:55:03 +0530

    Create Task_1.sol

---
+++ Task_1.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: GPL-3.0
+
+
+pragma solidity 0.8.0;
+
+
+contract Test{
+    function transferFunds(address _address, bytes calldata _payload) external{
+        (bool status,) = _address.delegatecall(_payload);
+        require(status, "Forwarded call failed.");
+    }
+}
